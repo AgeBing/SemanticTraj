877954eaf600a1c09fd66b41aba6e9623d985b82
高胜杰 <gsjhao@126.com>
2018-12-26 16:50:41 +0800

    NLP 服务迁移

--- site/backend/nlputil/nlp.py
+++ site/backend/nlputil/nlp.py
@@ -7,33 +7,35 @@ h/前接成分 k/后接成分 i/习语 j/简称
 r/代词 c/连词 p/介词 u/助词 y/语气助词
 e/叹词 o/拟声词 g/语素 w/标点 x/其它 
 """
-import gensim, logging
+# import gensim, logging
 import thulac
 import jieba
 
-from . import compare
+# from . import compare
 from backend import pathutil
 from backend import traj
+from . import nlpqueryutil
+import logging
 
 MAX_K_NUM = 1000
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # Load word2vec model, lazy load
 # model = None
-model = gensim.models.KeyedVectors.load_word2vec_format(pathutil.MODEL_PATH, binary=True)
+# model = gensim.models.KeyedVectors.load_word2vec_format(pathutil.MODEL_PATH, binary=True)
 # load thu lac
 thul = thulac.thulac()
 
-def get_word_vec(word):
-  try:
-    # if model is None:
-    #   model = gensim.models.KeyedVectors.load_word2vec_format(pathutil.MODEL_PATH, binary=True)
-    vec = model.get_vector(word)
-    return vec
-  except Exception as e:
-    print('word: ', word, ' has no vector')
-    return None
+# def get_word_vec(word):
+#   try:
+#     # if model is None:
+#     #   model = gensim.models.KeyedVectors.load_word2vec_format(pathutil.MODEL_PATH, binary=True)
+#     vec = model.get_vector(word)
+#     return vec
+#   except Exception as e:
+#     print('word: ', word, ' has no vector')
+#     return None
 
 def get_participle(sentence):
   return thul.cut(sentence)
@@ -48,14 +50,17 @@ def get_similiar_sites(sentence):
   sites = set()
   _site_cover = {}
   for i in range(0, len(words)):
-    word_vec = get_word_vec(words[i][0])
-    if word_vec is None:
-      continue
-    most_similiar_node = compare.get_max_k_node(word_vec, MAX_K_NUM)
+    # word_vec = get_word_vec(words[i][0])
+    # if word_vec is None:
+    #   continue
+    nlp_socket = nlpqueryutil.NlpSocket()
+    most_similiar_node = nlp_socket.query_nlp(words[i][0], MAX_K_NUM)
+    if len(most_similiar_node) == 0:
+      print(words, 'has no vector')
     for node in most_similiar_node:
-      sites.add(node.site_id)
-      state = _site_cover.get(node.site_id, 0)
-      _site_cover[node.site_id] = state | (1 << i)
+      sites.add(node['site_id'])
+      state = _site_cover.get(node['site_id'], 0)
+      _site_cover[node['site_id']] = state | (1 << i)
   logging.info('get_similiar_sites done!')
   return traj.Traj(len(words), sites, _site_cover)
   
---
+++ site/backend/nlputil/nlpqueryutil.py
@@ -0,0 +1,43 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import json
+import struct
+
+HOST = '10.76.0.183'
+PORT = 7777
+
+class NlpSocket:
+  def __init__(self):
+    self.sock = socket(AF_INET, SOCK_STREAM)
+
+  def connect(self, host, port):
+    self.sock.connect((host, port))
+
+  def send_msg(self, msg):
+    totalsent = 0
+    while True:
+      sent = self.sock.send(msg[totalsent:])
+      if sent == 0:
+        break
+      totalsent = totalsent + sent
+
+  def recvall(self, n):
+    data = b''
+    while len(data) < n:
+        packet = self.sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+  def receive_msg(self):
+    msg_len_raw = self.recvall(8)
+    msg_len = struct.unpack('>q', msg_len_raw)[0]
+    if msg_len is not None: 
+      raw_data = self.recvall(msg_len)
+      return raw_data.decode()
+
+  def query_nlp(self, word, K):
+    query_dict = {'type': 'query', 'word': word, 'K': K}
+    self.connect(HOST, PORT)
+    self.send_msg(json.dumps(query_dict).encode())
+    return json.loads(self.receive_msg())
